3e945bbea75b05ee474e854dd4bcdfc796601f56
Ron Harlev <harlev@gmail.com>
2024-11-06 20:05:54 -0800

    LLM class with mock

--- llms.py
+++ llms.py
@@ -3,23 +3,32 @@ from dotenv import load_dotenv
 from langchain_openai import ChatOpenAI
 from langchain_core.messages import HumanMessage, SystemMessage, BaseMessage
 from langchain_core.output_parsers import StrOutputParser
+from abc import ABC, abstractmethod
 
 load_dotenv()
 
-def openai_llm(messages: list[BaseMessage], model: str = "gpt-4o-mini"):
-    model = ChatOpenAI(model="gpt-4o-mini")
+class LLMInterface(ABC):
+    @abstractmethod
+    def generate(self, messages: list[BaseMessage], model: str = "gpt-4o-mini") -> str:
+        pass
 
-    parser = StrOutputParser()
-    chain = model | parser
+class OpenAILLM(LLMInterface):
+    def generate(self, messages: list[BaseMessage], model: str = "gpt-4o-mini") -> str:
+        model = ChatOpenAI(model=model)
+        parser = StrOutputParser()
+        chain = model | parser
+        return chain.invoke(messages)
 
-    return chain.invoke(messages)
-
-
+class MockLLM(LLMInterface):
+    def generate(self, messages: list[BaseMessage], model: str = "gpt-4o-mini") -> str:
+        return "Mock response"
 
 if __name__ == "__main__":
     messages = [
         SystemMessage(content="Translate the following from English into Italian"),
         HumanMessage(content="hi!"),
     ]
-    print(openai_llm(messages))
-
+    llm = OpenAILLM()
+    print(llm.generate(messages))
+    llm = MockLLM()
+    print(llm.generate(messages))
