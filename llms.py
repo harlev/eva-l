ea77fb4a9f1e1278c2ccbdb9618d4d3530141c87
Ron Harlev <harlev@gmail.com>
2024-11-06 20:16:18 -0800

    support generating based on object

--- llms.py
+++ llms.py
@@ -4,6 +4,7 @@ from langchain_openai import ChatOpenAI
 from langchain_core.messages import HumanMessage, SystemMessage, BaseMessage
 from langchain_core.output_parsers import StrOutputParser
 from abc import ABC, abstractmethod
+from pydantic import BaseModel
 
 load_dotenv()
 
@@ -18,10 +19,19 @@ class OpenAILLM(LLMInterface):
         parser = StrOutputParser()
         chain = model | parser
         return chain.invoke(messages)
+    
+    def generate_structured(self, messages: list[BaseMessage], pydantic_object: BaseModel, model: str = "gpt-4o-mini") -> str:
+        model = ChatOpenAI(model=model)
+        structured_llm = model.with_structured_output(pydantic_object)
+        return structured_llm.invoke(messages)
 
 class MockLLM(LLMInterface):
     def generate(self, messages: list[BaseMessage], model: str = "gpt-4o-mini") -> str:
         return "Mock response"
+    
+    def generate_structured(self, messages: list[BaseMessage], pydantic_object: BaseModel, model: str = "gpt-4o-mini") -> str:
+        fields = pydantic_object.model_fields.keys()
+        return pydantic_object(**{field: f"Mock {field} content" for field in fields})
 
 if __name__ == "__main__":
     messages = [
@@ -30,5 +40,17 @@ if __name__ == "__main__":
     ]
     llm = OpenAILLM()
     print(llm.generate(messages))
+    
+    class Joke(BaseModel):
+        setup: str
+        punchline: str
+
+    messages = [
+        HumanMessage(content="Tell me a joke about cats"),
+    ]
+
+    print(llm.generate_structured(messages, Joke))
+
     llm = MockLLM()
     print(llm.generate(messages))
+    print(llm.generate_structured(messages, Joke))
