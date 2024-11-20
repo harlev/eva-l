f69744a96b3db48ca9049d23791f2e9b9cfdd328
Ron Harlev <harlev@gmail.com>
2024-11-20 15:48:22 -0800

    add expander for regex

--- ui.py
+++ ui.py
@@ -7,9 +7,6 @@ from eval_types import RegexEvalScore
 from evals import generate
 
 
-if "variables_expanded" not in st.session_state:
-    st.session_state["variables_expanded"] = False
-
 def message(container, message, icon=None):
     # Display the success message in the container
     container.success(message, icon=icon)
@@ -22,7 +19,7 @@ def message(container, message, icon=None):
 
 selected_models = st.multiselect("Select models", ["gpt-4o-mini", "gpt-4o"])
 
-prompt = st.text_area("Enter a prompt", placeholder="What is the capital of {country}")
+prompt = st.text_area("Enter a prompt", placeholder="Prompt with {variables} here")
 
 @st.cache_data
 def process_csv(file):
@@ -34,7 +31,7 @@ def process_csv(file):
     except Exception as e:
         st.error(f"Error reading CSV file: {str(e)}")
 
-with st.expander("Variable Settings", expanded=st.session_state.variables_expanded):
+with st.expander("Variable Settings"):
     # CSV upload section
     uploaded_file = st.file_uploader("Upload variable CSV file", type="csv")
     if uploaded_file is not None:
@@ -48,13 +45,14 @@ with st.expander("Variable Settings", expanded=st.session_state.variables_expand
             columns,
             key="expected_column"
         )
-        regex_rule = st.text_input("Regex rule", value=r"^.*{expected}.*$")
-        regex_flags = sum((getattr(re, flag) for flag in st.multiselect("Regex flags", ["IGNORECASE"], default=["IGNORECASE"])), 0)
+
+with st.expander("Evaluation Settings"):
+    regex_rule = st.text_input("Regex rule", value=r"^.*{expected}.*$")
+    regex_flags = sum((getattr(re, flag) for flag in st.multiselect("Regex flags", ["IGNORECASE"], default=["IGNORECASE"])), 0)
 
 
 if st.button("Run"):
     with st.spinner("Running models..."):
-        st.session_state.variables_expanded = False
         df = st.session_state.edited_df
 
         eval = RegexEvalScore(rule=regex_rule, flags=regex_flags)
